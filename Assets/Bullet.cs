0e2b89e396f82ddd606538959604a30c8771bc89
amy-phillips <amy@islovely.co.uk>
2016-04-16 17:41:08 +0100

    health is networked, and bullets collide with and damage players

---
+++ Assets/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //GameObject[] players = GameObject.FindGameObjectsWithTag("PLAYER");
+   
+       // foreach (GameObject player in players) {
+       //     var combat = player.GetComponent<Combat>();
+       //     combat.TakeDamage(1);
+       // }
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<PlayerMovement>();
+        if (hitPlayer != null)
+        {
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(10);
+
+            Destroy(gameObject);
+        }
+    }
+}
